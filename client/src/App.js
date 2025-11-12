719b3dffaf6185c3d8e9756cea950fa83e53b70a
VanshSharma <vanshsharma2786@gmail.com>
2025-11-12 21:06:55 +0530

    Removed submodule and fixed client folder

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
+
+import memories from './images/memories.png';
+
+const App = () => {
+    return (
+        <Container maxWidth="lg">
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Memories</Typography>
+                <img src={memories} alt='memories' height='60'></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid xs={12} sm={7}>
+                            
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
+}
+
+export default App;
